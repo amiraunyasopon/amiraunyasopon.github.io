6d3d05f763161b4649140f3c98fef93a59c804f4
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-24 21:06:09 -0800

    style: tweaked footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex justify-between pb-5">
-            <p>This website was made with React, Tailwind, and Vite.</p>
+            <p>This website was made with React, Tailwind, and Vite</p>
             <a href="https://github.com/amiraunyasopon/amiraunyasopon.github.io" className="color-blue">View the source!</a>
         </div>
     )
