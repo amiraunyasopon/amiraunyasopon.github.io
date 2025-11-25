61d733be7bbb2ccb032d75426ac41fb1362c42d2
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-24 19:36:26 -0800

    style: added Zalando Sans font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold text-center">Amir Aunyasopon</h1>
+            <h1 className="text-3xl text-center font-ZalandoSans">Amir Aunyasopon</h1>
             <p>I'm a full-stack engineer and computer science student at UCI</p>
             <p>I love to learn, build for good, and attend hackathons</p>
         </div>
