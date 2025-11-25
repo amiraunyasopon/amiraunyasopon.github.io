a665f5cd5f2383d29e8463df618bf4695b9a56ee
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-24 20:41:42 -0800

    style: increased bottom padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between pb-5">
             <p>This website was made with React, Tailwind, and Vite.</p>
             <a href="https://github.com/amiraunyasopon/amiraunyasopon.github.io" className="color-blue">View the source!</a>
         </div>
