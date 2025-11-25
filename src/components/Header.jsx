da7812c1ee270fc82abf4b1b83f606b47a927d58
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-24 17:06:08 -0800

    feat: updated landing, revised header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold">Amir Aunyasopon</h1>
-            <p>I'm a full-stack developer engineer and computer science student at UCI</p>
-            <p>ex text</p>
+            <h1 className="text-3xl font-bold text-center">Amir Aunyasopon</h1>
+            <p>I'm a full-stack engineer and computer science student at UCI</p>
+            <p>I love to learn, build for good, and attend hackathons</p>
         </div>
     )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,18 +1,22 @@
 import Header from "./Header"
 import Experience from "./Experience"
+import Footer from "./Footer"
 const Landing = () => {
     return (
-        <div className="pagecentercontainer">
-            <div className="separator-div"></div>
-            <div className="pagecontent">
+        <div className="flex flex-col items-center min-h-screen">
+            <div className="">
                 <Header />
-                <div>
-                    <Experience text="position text" image="./vite.svg" imagealt="vite logo" />
-                    <Experience text="position 2 text" />
-                    <Experience text="position 3 text" />
+                <div className="separator-div"></div>
+                <div className="">
+                    <div>
+                        <Experience text="position text" image="./vite.svg" imagealt="vite logo" />
+                        <Experience text="position 2 text" />
+                        <Experience text="position 3 text" />
+                    </div>
                 </div>
+                <div className="separator-div"></div>
+                <Footer />
             </div>
-            <div className="separator-div"></div>
         </div>
     )
 }
