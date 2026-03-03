34b23e7258218ea075b0f5d04f138b9e92210ab7
amiraunyasopon <amiraunyasopon@gmail.com>
2026-03-02 22:37:40 -0800

    style: maintained consistency with grammar

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
                     <div className="flex-col">
                         <Experience title="Solutions, Consulting, & Engineering Intern" description="Delivery Insights & Optimization" company="World Wide Technology" link="https://www.wwt.com" time="June 2026 — August 2026" image="./wwt.svg" imagealt="World Wide Technology logo"></Experience>
                         <Experience title="Software Engineer Intern" description="Data Engineering (Cloud)" company="Opal" link="https://www.opal.community" time="December 2025 — May 2026" image="./opal.svg" imagealt="Opal logo" />
-                        <Experience title="Game Developer" description="Settings, shop, and player implementation" company="Dungeon Dilemma" link="https://vgdc-uci.framer.website" time="September 2025 — December 2025" image="./vgdc.svg" imagealt="Video Game Development Club at UCI logo" />
+                        <Experience title="Game Developer" description="Settings, Shop, & Player Implementation" company="Dungeon Dilemma" link="https://vgdc-uci.framer.website" time="September 2025 — December 2025" image="./vgdc.svg" imagealt="Video Game Development Club at UCI logo" />
                         <Experience title="Data Analytics Intern" description="Administrative & Literacy Services" company="City of Burbank" link="https://www.burbankca.gov" time="June 2025 — August 2025" image="./cityofburbank.svg" imagealt="City of Burbank logo" />
                     </div>
                 </div>
--- src/components/Organizations.jsx
+++ src/components/Organizations.jsx
@@ -11,8 +11,8 @@ const Organizations = () => {
                     <div className="flex-col">
                         <Experience title="President" description="Supporting students technically & professionally" company="Google Developer Group on Campus at UCI" link="https://gdg.community.dev" time="Dec 2025 — Present" image="./googledevelopergroups.svg" imagealt="Google Developer Groups Logo"></Experience>
                         <Experience title="Academic and Alumni Affairs Committee Member" description="Coordinating mentorship and learning opportunities" company="ICS Student Council" link="https://studentcouncil.ics.uci.edu" time="Oct 2025 — Present" image="./icssc.svg" imagealt="Informatics and Computer Science Student Council Logo"></Experience>
-                        <Experience title="Web Development Fellow" description="Collaborating & Learning Full Stack" company="CodePath" link="https://www.codepath.org" time="Oct 2025 — Nov 2025" image="./codepathorg.svg" imagealt="CodePath Logo"></Experience>
-                        <Experience title="Engineering Track" description="Early-Career Enrichment" company="National Academy Foundation" link="https://naf.org" time="2021 — 2025" image="./naforg.svg" imagealt="National Academy Foundation Logo"></Experience>
+                        <Experience title="Web Development Fellow" description="Collaborating & learning full stack" company="CodePath" link="https://www.codepath.org" time="Oct 2025 — Nov 2025" image="./codepathorg.svg" imagealt="CodePath Logo"></Experience>
+                        <Experience title="Engineering Track" description="Early-career enrichment" company="National Academy Foundation" link="https://naf.org" time="2021 — 2025" image="./naforg.svg" imagealt="National Academy Foundation Logo"></Experience>
                     </div>
                 </div>
                 <div className="border-t border-gray-300 my-5"></div>
