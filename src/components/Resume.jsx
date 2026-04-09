cf5d594533bc8e8b0f8f751295d38ce363ab5bc0
amiraunyasopon <amiraunyasopon@gmail.com>
2026-04-08 19:02:40 -0700

    style: removed typo preventing resume header from being centered

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
     return (
         <div className="flex flex-col items-center min-h-screen">
             <div className="w-full max-w-screen-lg px-4 sm:px-8">
-                <h1 className="text-3xl te`xt-center">Resume</h1>
+                <h1 className="text-3xl text-center">Resume</h1>
                 <div className="border-t border-gray-300 my-5"></div>
                 <div className="w-full h-[80vh]">
                     <iframe
